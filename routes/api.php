d488cae189d4866c12a5695935101bc034a78ee0
Hanif Al Fathoni <alfatoni922@gmail.com>
2024-10-23 02:39:42 +0700

    api: Api Resource

--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,4 @@ use App\Http\Controllers\Api\BookController;
 |
 */
 
-Route::get('/books', [BookController::class, 'index']);
-Route::post('/books', [BookController::class, 'store']);
-Route::get('/books/{book}', [BookController::class, 'show']);
-Route::put('/books/{book}', [BookController::class, 'update']);
-Route::delete('/books/{book}', [BookController::class, 'destroy']);
+Route::apiResource('books', BookController::class);
